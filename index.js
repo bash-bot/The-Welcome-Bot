fb31795cf7a21f5ee084c78df8443aff02223537
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-03-02 20:30:39 +0530

    clients can have their custom messages
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

--- index.js
+++ index.js
@@ -24,11 +24,30 @@ module.exports = (robot) => {
       }
     }).then(function (user) {
       if (user === null) {
-        db.models.create({
+        db.User.create({
           username: context.payload.sender.login,
           repositoryName: context.payload.repository.full_name
-        }).then(user => {
-          return context.github.issues.createComment(params)
+        }).then(async user => {
+
+          console.log("reach")
+
+          try {
+            console.log("reached1")
+            const config = await context.config('config.yml');
+            console.log("reached2")
+
+            if (config.welcomeMessage) {
+              console.log("reached3")
+
+              context.github.issues.createComment(context.issue({body: config.welcomeMessage}))
+            }
+          } catch (err) {
+            if (err.code !== 404) {
+              throw err;
+            }
+          }
+
+
         }).catch(function (err) {
           console.log(err);
         })
@@ -60,11 +79,25 @@ module.exports = (robot) => {
       }
     }).then(function (user) {
       if (user === null) {
-        db.models.create({
+        db.User.create({
           username: context.payload.comment.user.login,
           repositoryName: context.payload.repository.full_name
         }).then(user => {
-          return context.github.issues.createComment(params)
+
+          try {
+            const config = context.config('config.yml');
+            if (config.welcomeMessage) {
+              return context.github.issues.createComment(context.issue({body: config.welcomeMessage}))
+            }
+          } catch (err) {
+            if (err.code !== 404) {
+              throw err;
+            }
+          }
+
+
+
+
         }).catch(function (err) {
           console.log(err);
         })
@@ -96,11 +129,22 @@ module.exports = (robot) => {
       }
     }).then(function (user) {
       if (user === null) {
-        db.models.create({
+        db.User.create({
           username: context.payload.sender.login,
           repositoryName: context.payload.repository.full_name
         }).then(user => {
-          return context.github.issues.createComment(params)
+
+          try {
+            const config = context.config('config.yml');
+            if (config.welcomeMessage) {
+              return context.github.issues.createComment(context.issue({body: config.welcomeMessage}))
+            }
+          } catch (err) {
+            if (err.code !== 404) {
+              throw err;
+            }
+          }
+
         }).catch(function (err) {
           console.log(err);
         })
