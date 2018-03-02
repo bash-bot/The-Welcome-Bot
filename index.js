45c2c3c73cc0c67ae48b30869552d158a115007f
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-03-02 20:37:39 +0530

    formatting
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 const db = require('./lib/schema').models;
 
-//let arr = ["the-welcome-bot[bot]"]
-
 module.exports = (robot) => {
+
   robot.log('Yay, the app was loaded!')
 
   robot.on('issues.opened', async context => {
-    const params = context.issue({body: 'Hello World!'})
-    // console.log(params)
-    // console.log('********')
-    // if (arr.indexOf(context.payload.sender.login) !== -1) {
-    //   console.log(arr)
-    // } else {
-    //   arr.push(context.payload.sender.login)
-    //   console.log(arr)
-    //   return context.github.issues.createComment(params)
-    // }
 
     db.User.findOne({
       where: {
@@ -59,18 +48,6 @@ module.exports = (robot) => {
 
   })
   robot.on('issue_comment.created', async context => {
-     const params = context.issue({body: 'Hello World!'})
-    // console.log(context.payload.comment.user.login)
-    // // console.log(context.payload.comment.user.login)
-    // console.log('********')
-    // // console.log(params)
-    // if (arr.indexOf(context.payload.comment.user.login) !== -1) {
-    //   console.log(arr)
-    // } else {
-    //   arr.push(context.payload.comment.user.login)
-    //   console.log(arr)
-    //   return context.github.issues.createComment(params)
-    // }
 
     db.User.findOne({
       where: {
@@ -96,8 +73,6 @@ module.exports = (robot) => {
           }
 
 
-
-
         }).catch(function (err) {
           console.log(err);
         })
@@ -107,20 +82,6 @@ module.exports = (robot) => {
     })
   })
   robot.on('pull_request.opened', async context => {
-    const params = context.issue({body: 'Hello World!'})
-    // console.log(context)
-    // // console.log(context.payload.comment.user.login)
-    // console.log('********')
-    // // console.log(params)
-    // if (arr.indexOf(context.payload.sender.login) !== -1) {
-    //   console.log(arr)
-    // } else {
-    //   arr.push(context.payload.sender.login)
-    //   console.log(arr)
-    //   // console.log(context.github)
-    //   return context.github.issues.createComment(params)
-    // }
-
 
     db.User.findOne({
       where: {
