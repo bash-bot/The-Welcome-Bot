9a44011a3ac3052c9a711ce6451f334933d4c8fd
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-03-04 22:56:39 +0530

    fixed up error
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = (robot) => {
       , repositoryName = context.payload.repository.full_name;
     console.log("**************************")
     console.log(context)
-    if(username !== "the-welcome-bot[bot]")
+    if (username !== "the-welcome-bot[bot]")
       checkUser(context, username, repositoryName, "issueOpen")
 
   })
@@ -20,7 +20,7 @@ module.exports = (robot) => {
     let username = context.payload.comment.user.login
       , repositoryName = context.payload.repository.full_name;
 
-    if(username !== "the-welcome-bot[bot]")
+    if (username !== "the-welcome-bot[bot]")
       checkUser(context, username, repositoryName, "issueComment")
 
   })
@@ -31,7 +31,7 @@ module.exports = (robot) => {
       , repositoryName = context.payload.repository.full_name;
     console.log("**************************")
     console.log(context)
-    if(username !== "the-welcome-bot[bot]")
+    if (username !== "the-welcome-bot[bot]")
       checkUser(context, username, repositoryName, "prOpen")
 
   })
@@ -60,18 +60,18 @@ module.exports = (robot) => {
             console.log("**************************")
 
             const config = await context.config('config.yml');
-console.log(config)
+            console.log(config)
             let message;
-
-            switch (eventType) {
-              case "issueOpen":
-                message = config.issueOpen || config.welcomeMessage;
-              case "issueComment":
-                message = config.issueComment || config.welcomeMessage;
-              case "prOpen":
-                message = config.prOpen || config.welcomeMessage;
+            if (config) {
+              switch (eventType) {
+                case "issueOpen":
+                  message = config.issueOpen || config.welcomeMessage;
+                case "issueComment":
+                  message = config.issueComment || config.welcomeMessage;
+                case "prOpen":
+                  message = config.prOpen || config.welcomeMessage;
+              }
             }
-
             if (message) {
               return context.github.issues.createComment(context.issue({body: message}))
             } else {
