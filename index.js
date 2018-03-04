69ca6bbe83783f282ac8884b9dae7e8cb351d81b
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-03-04 20:50:39 +0530

    cleaned up code and custom messages for issue open, comment and pr open
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

--- index.js
+++ index.js
@@ -6,113 +6,98 @@ module.exports = (robot) => {
 
   robot.on('issues.opened', async context => {
 
-    db.User.findOne({
-      where: {
-        username: context.payload.sender.login,
-        repositoryName: context.payload.repository.full_name
-      }
-    }).then(function (user) {
-      if (user === null) {
-        db.User.create({
-          username: context.payload.sender.login,
-          repositoryName: context.payload.repository.full_name
-        }).then(async user => {
-          try {
-            const config = await context.config('config.yml');
-            if (config.welcomeMessage) {
-              context.github.issues.createComment(context.issue({body: config.welcomeMessage}))
-            }
-          } catch (err) {
-            if (err.code !== 404) {
-              throw err;
-            }
-          }
+    let username = context.payload.sender.login
+      , repositoryName = context.payload.repository.full_name;
 
-
-        }).catch(function (err) {
-          console.log(err);
-        })
-      }
-    }).catch(function (err) {
-      console.log(err);
-    })
-
+    checkUser(username, repositoryName, issueOpen)
 
   })
+
   robot.on('issue_comment.created', async context => {
 
-    db.User.findOne({
-      where: {
-        username: context.payload.comment.user.login,
-        repositoryName: context.payload.repository.full_name
-      }
-    }).then(function (user) {
-      if (user === null) {
-        db.User.create({
-          username: context.payload.comment.user.login,
-          repositoryName: context.payload.repository.full_name
-        }).then(async user => {
-          console.log("reahed1")
-          console.log(user)
-          try {
-            console.log("reached2")
-            const config = await context.config('config.yml');
-            console.log(config)
-            if (config.welcomeMessage) {
-              console.log("reached3")
-              console.log(config.welcomeMessage)
-              return context.github.issues.createComment(context.issue({body: config.welcomeMessage}))
-            }  else {
-              console.log("hey there")
-            }
-          } catch (err) {
-            if (err.code !== 404) {
-              throw err;
-            }
-          }
+    let username = context.payload.comment.user.login
+      , repositoryName = context.payload.repository.full_name;
 
+    checkUser(username, repositoryName, issueComment)
 
-        }).catch(function (err) {
-          console.log(err);
-        })
-      }
-    }).catch(function (err) {
-      console.log(err);
-    })
   })
+
   robot.on('pull_request.opened', async context => {
 
+    let username = context.payload.sender.login
+      , repositoryName = context.payload.repository.full_name;
+
+    checkUser(username, repositoryName, prOpen)
+
+  })
+
+  function checkUser(username, repositoryName, botCommentFunction) {
     db.User.findOne({
       where: {
-        username: context.payload.sender.login,
-        repositoryName: context.payload.repository.full_name
+        username: username,
+        repositoryName: repositoryName
       }
     }).then(function (user) {
       if (user === null) {
         db.User.create({
-          username: context.payload.sender.login,
-          repositoryName: context.payload.repository.full_name
-        }).then(async user => {
-
-          try {
-            const config = await context.config('config.yml');
-            if (config.welcomeMessage) {
-              return context.github.issues.createComment(context.issue({body: config.welcomeMessage}))
-            }
-          } catch (err) {
-            if (err.code !== 404) {
-              throw err;
-            }
-          }
-
-        }).catch(function (err) {
-          console.log(err);
-        })
+          username: username,
+          repositoryName: repositoryName
+        }).then(botCommentFunction)
+          .catch(function (err) {
+            console.log(err);
+          })
       }
     }).catch(function (err) {
       console.log(err);
     })
-  })
+  }
 
+  async function issueOpen() {
+    try {
+      const config = await context.config('config.yml');
+      let message = config.issueOpen || config.welcomeMessage;
+      if (message) {
+        return context.github.issues.createComment(context.issue({body: message}))
+      } else {
+        return context.github.issues.createComment(context.issue({body: "Hello World! Welcome to this project."}))
+      }
+    } catch (err) {
+      if (err.code !== 404) {
+        throw err;
+      }
+    }
+  }
+
+  async function issueComment() {
+    try {
+      const config = await context.config('config.yml');
+      let message = config.issueComment || config.welcomeMessage;
+      if (message) {
+        return context.github.issues.createComment(context.issue({body: message}))
+      } else {
+        return context.github.issues.createComment(context.issue({body: "Hello World! Welcome to this project."}))
+      }
+    } catch (err) {
+      if (err.code !== 404) {
+        throw err;
+      }
+    }
+  }
+
+  async function prOpen() {
+    try {
+      const config = await context.config('config.yml');
+      let message = config.prOpen || config.welcomeMessage;
+      if (message) {
+        return context.github.issues.createComment(context.issue({body: message}))
+      } else {
+        return context.github.issues.createComment(context.issue({body: "Hello World! Welcome to this project."}))
+      }
+    } catch (err) {
+      if (err.code !== 404) {
+        throw err;
+      }
+    }
+  }
 
 }
